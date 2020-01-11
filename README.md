6fa5d82bc01cbb39969c2f892d79c273fb326d85
Colo <colo@MacBook-Pro-de-Colo.local>
2020-01-11 14:57:50 -0300

    Fixing meta information

--- README.md
+++ README.md
@@ -1,14 +1,5 @@
-<!-- AUTO-GENERATED-CONTENT:START (STARTER) -->
-<p>
-  <a href="https://mountains-hj.netlify.com/">
-    <h1>Mountains</h1>
-  </a>
-</p>
+## Mountains
 
-<p style="margin-bottom: 0">
-  Mountains is a Progressive Web App made with [Gatsby](https://www.gatsbyjs.org).
-</p>
-<p>
+Mountains is a Progressive Web App made with [Gatsby](https://www.gatsbyjs.org).
+
   You can access [clicking here](https://mountains-hj.netlify.com), add it to your main screen and feel it like a real app!
-</p>
-<!-- AUTO-GENERATED-CONTENT:END -->
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Mountains`,
+    title: `Mountains by Juan Helbert`,
     description: `Mountains is a Progressive Web App made with Gatsby. You can add it to your main screen and feel it like a real app!`,
     author: `Juan Helbert`,
   },
@@ -24,11 +24,9 @@ module.exports = {
         background_color: `#131117`,
         theme_color: `#131117`,
         display: `standalone`,
-        icon: `src/images/mountains.png`, // This path is relative to the root of the site.
+        icon: `src/images/mountains.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
   ],
 }
